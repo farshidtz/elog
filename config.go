68028a26aece5465a10101f8e2b92f83d68dc2fa
Farshid Tavakolizadeh <farshid.tz@gmail.com>
2016-07-20 13:15:18 +0200

    added docs

--- config.go
+++ config.go
@@ -11,19 +11,32 @@ import (
 
 const (
 	NoTrace   = 1              // no file name or line lumber
-	LongFile  = log.Llongfile  // full file name and line number: /a/b/c/d.go:23
-	ShortFile = log.Lshortfile // final file name element and line number: d.go:23
+	LongFile  = log.Llongfile  // absolute file path and line number: /home/user/go/main.go:15
+	ShortFile = log.Lshortfile // file name and line number: main.go:15
 )
 
 type Config struct {
+	// Writer is the destination to which log data is written
+	// Default: os.Stdout
 	Writer       io.Writer
+	// TimeFormat is the format of time in the prefix
+	// Default: "2006/01/02 15:04:05"
 	TimeFormat   string
-	Prefix       string
+	// Trace is to control the tracing information
+	// Default: NoTrace
 	Trace        int
+	// DebugEnabled can be used to pass a parsed command-line boolean flag to enable the debugging
+	// Default: nil
 	DebugEnabled *bool
-	DebugPrefix  string
-	DebugTrace   int
+	// DebugEnvVar is the environment variable used to enable debugging when set to 1
+	// Default: "DEBUG"
 	DebugEnvVar  string
+	// DebugPrefix is the prefix used when logging with Debug methods and Errorf
+	// Default: "[debug] "
+	DebugPrefix  string
+	// DebugTrace is to control the tracing information when in debugging mode
+	// Default: ShortFile
+	DebugTrace   int
 }
 
 func initConfig(config *Config) *Config {
