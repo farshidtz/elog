f8d5da407dca21c0ff0303d0beff6b662b641a81
Farshid Tavakolizadeh <email@farshid.ws>
2017-12-18 11:11:43 +0100

    created getter for unexported writer

--- writer.go
+++ writer.go
@@ -13,3 +13,11 @@ type writer struct {
 func (w writer) Write(b []byte) (n int, err error) {
 	return w.Writer.Write(append([]byte(time.Now().Format(w.timeFormat)), b...))
 }
+
+// NewWriter returns an io.Writer that writes timestamps as prefix
+func NewWriter(writer io.Writer, timeFormat string) *writer {
+	if timeFormat == "" {
+		timeFormat = "2006-01-02 15:04:05 "
+	}
+	return &writer{writer, timeFormat}
+}
