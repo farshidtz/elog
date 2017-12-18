122e062de95f1f03e80baa633e2e62e21bcc80c9
Farshid Tavakolizadeh <email@farshid.ws>
2017-12-18 11:22:40 +0100

    created getter for unexported writer (2)

--- writer.go
+++ writer.go
@@ -14,10 +14,20 @@ func (w writer) Write(b []byte) (n int, err error) {
 	return w.Writer.Write(append([]byte(time.Now().Format(w.timeFormat)), b...))
 }
 
-// NewWriter returns an io.Writer that writes timestamps as prefix
-func NewWriter(writer io.Writer, timeFormat string) *writer {
-	if timeFormat == "" {
-		timeFormat = "2006-01-02 15:04:05 "
+/*
+NewWriter returns a new io.Writer that writes timestamps as prefix
+
+E.g. usage:
+
+	logger := log.New(elog.NewWriter(os.Stdout), "[info] ", 0)
+	logger.Println("Hello.")
+
+	Prints: 2016/07/19 17:34:10 [info] Hello.
+*/
+func NewWriter(w io.Writer, timeFormat ...string) *writer {
+	tf := "2006-01-02 15:04:05 "
+	if len(timeFormat) == 1 {
+		tf = timeFormat[0]
 	}
-	return &writer{writer, timeFormat}
+	return &writer{w, tf}
 }
